965c946d152282bf19f717145244337acde145f8
fromljs <fromjs@gmail.com>
2022-10-06 15:01:17 +0900

    upd

--- test01.py
+++ test01.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import shutil
 
 
-def imgload(pathdir, Filename):
+def imgload(pathdir, Filename, model):
     imgFilename = pathdir + '\\' + Filename
     ngFilename = _ngDirname + '\\' + Filename
     okFilename = _okDirname + '\\' + Filename
@@ -23,11 +23,11 @@ def imgload(pathdir, Filename):
     ng = prediction.T[1]
 
     # print(prediction)
-    if  (ok < ng) and (ng > 0.9)  : 
+    if  (ok < ng) and (ng > 0.5)  : 
         print(imgFilename + ' : ng', ng)
         shutil.copyfile(imgFilename, ngFilename)
-        # with open(savefilename, mode='a', encoding='utf-8') as file:
-        #     file.write('{0} {1} \n'.format(imgFilename , ng))        
+        with open(_savefilename, mode='a', encoding='utf-8') as file:
+            file.write('{0} {1} \n'.format(imgFilename , ng))        
     elif (ok > ng) and (ok < 0.6):
         print(imgFilename + ' : ok', ok)
         shutil.copyfile(imgFilename, okFilename)
@@ -35,24 +35,28 @@ def imgload(pathdir, Filename):
 def loadImg(path):
     # path = r'C:\Users\jslee\Desktop\새 폴더\세방이미지\현대신형-L70\Right'
     list = os.listdir(path)
-    # savefilename = 'test_' + str(datetime.now())[0:10] + '.txt'
+
 
     for i in range(len(list)):
-        if list[i].find('.jpg') > 0 :  
-            imgload(path, list[i])
+        if list[i].find('0.jpg') > 0 :  
+            imgload(path, list[i], model_L)
+        # elif list[i].find('1.jpg') > 0 :  
+        #     imgload(path, list[i], model_R)
 
 
 
 
 ########### Main ############
-loadDir = r'C:\Users\jslee\Desktop\새 폴더\세방이미지'
-_ngDirname = r'C:\Users\jslee\Desktop\새 폴더\NG'
-_okDirname = r'C:\Users\jslee\Desktop\새 폴더\OK' 
+loadDir = r'C:\Users\jslee\Desktop\세방전지\세방이미지'
+_ngDirname = r'C:\Users\jslee\Desktop\세방전지\NG'
+_okDirname = r'C:\Users\jslee\Desktop\세방전지\OK' 
+_savefilename = 'test_' + str(datetime.now())[0:10] + '.txt'
 if not os.path.exists(_ngDirname): os.mkdir(_ngDirname)
 if not os.path.exists(_okDirname): os.mkdir(_okDirname)
 
 
-model = load_model('keras_model.h5')
+model_L = load_model('keras_model_L.h5')
+model_R = load_model('keras_model_R.h5')
 for (path, dir, files) in os.walk(loadDir):
     for dirname in dir:        
         loadImg(path + '\\' + dirname)
